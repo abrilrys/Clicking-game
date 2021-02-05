e34b0fd33f3942d4665a1d3af1e10e2d8d67eed1
Abril <abrilseyer12@gmail.com>
2021-02-05 16:46:08 -0600

    first commit

---
+++ Assets/Scripts/Coins.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class Coins : MonoBehaviour
+{
+    public Text message;
+    private int add;
+
+    public void coinsup()
+    {
+        add++;
+        message.text = add.ToString();
+    }
+}
