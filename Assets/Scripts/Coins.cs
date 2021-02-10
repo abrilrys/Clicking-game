5dc2f789c375088e59a2dab920758d70db56e413
Abril <abrilseyer12@gmail.com>
2021-02-10 16:14:58 -0600

    little fix on buttons #1

--- Assets/Scripts/Coins.cs
+++ Assets/Scripts/Coins.cs
@@ -2,14 +2,48 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+
 public class Coins : MonoBehaviour
 {
     public Text message;
-    private int add;
+    public float add;
+    public Button UpgradeButton;
+
+
+
+    void start()
+    {
+        UpgradeButton.interactable = !UpgradeButton.interactable;
+        
+    }
 
     public void coinsup()
     {
+       
         add++;
-        message.text = add.ToString();
+        message.text = (int)add + " coins";
+        
+
     }
+
+    void Update()
+    {
+        if (add < 50)
+        {
+            UpgradeButton.interactable = false;
+        }
+        else
+        {
+            UpgradeButton.interactable = true;
+        }
+    }
+    //void TaskOnClick()
+    //{
+    //    print("pressed");
+    //    add -= 50;
+    //    add += pointsincreasedpersec * Time.deltaTime;
+    //    message.text = (int)add + " coins";
+    //}
+
+
 }
---
+++ Assets/Scripts/UpgradeButton.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UpgradeButton : MonoBehaviour
+{
+    public Text message1;
+    public float add;
+  
+    public float pointsincreasedpersec1;
+
+
+
+
+    
+
+    public void Coinspersec()
+    {
+        add=GameObject.Find("Canvas").GetComponent<Coins>().add;
+        add -=50;
+        InvokeRepeating("coins", 1f, 1f);
+        message1.text = (int)add + "  coins";
+        GameObject.Find("Canvas").GetComponent<Coins>().add= add;
+
+        
+    }
+    void coins()
+    {
+        add+= 1;
+    }
+}
